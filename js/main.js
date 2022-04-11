91de82f946d6de670d9ea7da64229781d46480d2
atp3 <atp3@aber.ac.uk>
2022-04-11 19:15:41 +0100

    add website

---
+++ js/main.js
@@ -0,0 +1,50 @@
+const nav = document.querySelector('.nav');
+const navBtn = document.querySelector('.burger-btn');
+const allNavItems = document.querySelectorAll('.nav__item');
+const navBtnBars = document.querySelector('.burger-btn_bars');
+const allSections = document.querySelectorAll('.section');
+const footerYear = document.querySelector('.footer__year');
+
+
+const handleNav = () => {
+    nav.classList.toggle('nav--active')
+
+    allNavItems.forEach(item=>{
+        item.addEventListener('click', ()=>{
+            nav.classList.remove('nav--active')
+        })
+    })
+    handleNavItemsAnimation();
+}
+
+const handleNavItemsAnimation = () =>{
+    let delaytime = 0;
+
+    allNavItems.forEach(item =>{
+        item.classList.toggle('nav-items-animation')
+        item.style.animationDelay = '.' + delaytime + 's';
+        delaytime ++;
+    })
+}
+
+const handleCurrentYear = () =>{
+    const year = (new Date).getFullYear();
+    footerYear.innerText = year;
+}
+
+const handleObserver = () =>{
+    const currentSection = window.scrollY;
+
+    allSections.forEach(section=>{
+
+        if(section.classList.contains('white-section')&& section.offsetTop <= currentSection + 60){
+            navBtnBars.classList.add('black-bars-color')
+        }else if(!section.classList.contains('white-section')&& section.offsetTop <= currentSection + 60){
+            navBtnBars.classList.remove('black-bars-color')
+        }
+    })
+}
+
+handleCurrentYear();
+navBtn.addEventListener('click', handleNav)
+window.addEventListener('scroll', handleObserver)
